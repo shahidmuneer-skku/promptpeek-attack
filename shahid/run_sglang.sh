c93e01fea8d81b6c0461553afaf2533830862908
chaejin98330 <chaejin98@g.skku.edu>
2025-12-15 12:09:40 +0900

    chaejin

--- shahid/run_sglang.sh
+++ shahid/run_sglang.sh
@@ -4,10 +4,8 @@ export SGL_ENABLE_JIT_DEEPGEMM=0
 python -m sglang.launch_server \
   --model Qwen/Qwen3-0.6B-FP8 \
     --attention-backend triton \
-  --mem-fraction-static 0.85 \
+  --mem-fraction-static 0.75 \
   --schedule-policy lpm \
-  --enable-deterministic-inference \
-  --cuda-graph-max-bs 16 \
-  --disable-cuda-graph
+  --enable-deterministic-inference
   
 #   --attention-backend flashinfer \
